c282d4cfe4392832c43369f6492a45ca3d2d7f14
lufftewaffle <leeando90@gmail.com>
2024-10-08 21:49:33 +1300

    added functions in auth.py

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -13,6 +13,7 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        email = request.form['email']
         db = get_db()
         error = None
         
@@ -22,9 +23,13 @@ def register():
         elif not password:
             error = 'Password is required'
             
+        elif not email:
+            error = 'Email is requiredd'
+            
         if error is None:
             try:
-                db.execute("INSERT INTO user (username, password) VALUES (?, ?)", (username, generate_password_hash(password)),)
+                db.execute("INSERT INTO user (username, password, email) VALUES (?, ?, ?)", 
+                           (username, generate_password_hash(password), email),)
                 db.commit()
             except db.IntegrityError:
                 error = f"User {username} already exists"
@@ -34,3 +39,43 @@ def register():
             
         flash(error)
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        db = get_db()
+        error = None
+        
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone() # returns one row from query.
+            # If the query doesn't return a result, it returns None
+        if user is None:
+            error = 'Incorrect Username'
+        elif not check_password_hash(user[password], password): # check_password_hash hashes submitted pw in the same manner as the
+            # stored hash, and if they match, the PW is valid.
+            error = 'Incorrect Password'
+            
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+        
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id)).fetchone()
+        
+        
+@bp.route('/logout')
+def log_out():
+    session.clear()
+    return redirect(url_for('index'))
