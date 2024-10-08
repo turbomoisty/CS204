059892988efff41e72ce5dbbdc1cb4bb49ead893
lufftewaffle <leeando90@gmail.com>
2024-10-08 22:35:35 +1300

    not working???

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,8 +27,13 @@ def create_app(test_config=None):
     except OSError:
         pass
     
+    
     from . import db
     db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+        
 
     @app.route('/')
     def hello():
