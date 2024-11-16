e7500e34852befeb9af50fda1cbff5e7030b7a3e
lufftewaffle <leeando90@gmail.com>
2024-11-16 17:11:24 +1300

    added ability to drag and dorp

---
+++ flaskr/static/file_encrypt.js
@@ -0,0 +1,34 @@
+const drop_area = document.getElementById('drop_area');
+const input_file = document.getElementById('input_file');
+const file_view = document.getElementById('file_view');
+
+input_file.addEventListener('change',uploadFile);
+
+
+function uploadFile(){
+        const file = input_file.files[0];
+
+        if(file){
+    
+            let link = URL.createObjectURL(file);
+    
+            file_view.style.backgroundImage = `url(${link})`;
+            file_view.querySelector('p').textContent = file.name;
+            alert(`--${file.name}-- has been sucessfully uploaded`)
+            file_view.querySelector('img').style.display = '';
+            
+
+            drop_area.addEventListener('dragover', (e)=>{
+                e.preventDefault();
+            
+            });
+            
+            drop_area.addEventListener('drop', (e)=>{
+                e.preventDefault();
+                input_file.files = e.dataTransfer.files;
+                uploadFile();
+            });
+       
+        }
+    };
+    
