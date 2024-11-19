c4b0bd5ad9e8fe0a511115e7628aa0498bd7f356
lufftewaffle <leeando90@gmail.com>
2024-11-20 11:11:52 +1300

    Redesigned some elements of login pages

--- flaskr/static/main_page.js
+++ flaskr/static/main_page.js
@@ -10,15 +10,14 @@ Array.from(document.getElementsByClassName('menu-item')).forEach((item, index) =
         };
     });
 
-    text_box.forEach(box =>{ // Makes sure all boxes are hidden by default.
-        box.style.display = 'none';
+    text_box.forEach(t_box =>{ // Makes sure all boxes are hidden by default.
+        t_box.style.display = 'none';
     });
 
 for (let i =0; i < icon_items.length; i++){
     icon_items[i].addEventListener('mouseover', ()=>{
-
-        text_box.forEach(box =>{
-            box.style.display = 'none';
+        text_box.forEach(t_box =>{
+            t_box.style.display = 'none';
         });
 
         setTimeout(function(){
@@ -33,6 +32,7 @@ for (let i =0; i < icon_items.length; i++){
     // This whole portion is sort of janky, but it works.
     if (text_box[i]){
         icon_items[i].addEventListener('mouseleave', () => {
+            text_box[i].style.animation = 'pop-up 233ms ease-in-out reverse';
             text_box[i].style.display = 'none';
         });
     };
