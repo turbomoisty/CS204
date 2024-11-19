0e7ee84499b0ae9d81218e5e8d291dfa5019004e
lufftewaffle <leeando90@gmail.com>
2024-11-19 16:03:10 +1300

    fixed animation issue with text box not materialiseing correctly, fixed alignment issue with text box not matching up with icon, added text description within text box.

--- flaskr/static/main_page.js
+++ flaskr/static/main_page.js
@@ -1,12 +1,40 @@
 const menu = document.getElementById('menu');
-const icon_items = document.querySelectorAll('.icon_item');
-const text_box = document.querySelectorAll('#text_box_display');
+const icon_items = document.getElementsByClassName('icon-item');
+const text_box = document.querySelectorAll('#picture-list > #text_box_display');
 
+
+//Array for the left text to ensure scrolling works as intended
 Array.from(document.getElementsByClassName('menu-item')).forEach((item, index) => {
         item.onmouseover =() =>{
             menu.dataset.activeIndex = index;
         };
     });
 
+    text_box.forEach(box =>{ // Makes sure all boxes are hidden by default.
+        box.style.display = 'none';
+    });
 
+for (let i =0; i < icon_items.length; i++){
+    icon_items[i].addEventListener('mouseover', ()=>{
+
+        text_box.forEach(box =>{
+            box.style.display = 'none';
+        });
+
+        setTimeout(function(){
+            if (text_box[i]) {
+                text_box[i].style.animation = 'pop-up 333ms ease-in-out forwards';
+                text_box[i].style.display = 'block';
+            }
+        },100)
+
+    });
+
+    // This whole portion is sort of janky, but it works.
+    if (text_box[i]){
+        icon_items[i].addEventListener('mouseleave', () => {
+            text_box[i].style.display = 'none';
+        });
+    };
+}
 
